5f4584318fbf3d7b1601ed88775a7c15ce13e966
Darlan Schmeller <darlanschimeller@gmail.com>
2024-04-19 13:40:43 -0300

    started studies with databases

--- formulario/formulario.php
+++ formulario/formulario.php
@@ -7,20 +7,81 @@ crossorigin="anonymous">
 <h2 class="cadastro">Cadastro</h2>
 
 <hr>
+
+<?php
+// Ao enviar o formulário o mesmo redireciona para #, sendo o próprio site, isso acontece 
+// por causa do form action="#"
+// então como o $_POST > 0 o código abaixo roda.
+
+if(count($_POST) > 0) { // Count() conta a quantidade de valores, se for maior que 0 significa
+// que foi criado valores dentro de post
+
+    $erros = []; // Define um array responsável por armazenar os erros.
+
+    if(!filter_input(INPUT_POST, "nome")) {
+        $erros['nome'] = 'Nome inválido.';
+    }
+    
+    if(!filter_input(INPUT_POST, "nascimento")) { // que foi criado valores dentro de post
+        $data = DateTime::createFromFormat( // função estática nativa do PHP
+            'd/m/Y', $_POST["nascimento"]);
+        if(!$data) {
+            $erros['nascimento'] = 'Data deve estar no padrão dd/mm/aaaa.';
+        }
+    }
+   
+     /* filter_input() -> utilizada para recuperar uma variável externa específica e, 
+     opcionalmente, aplicar um ou mais filtros a ela. Essa função é comumente usada 
+     para obter dados do ambiente externo, como entradas do usuário (por exemplo, dados
+     de formulário enviados via POST ou GET) e filtrá-los para garantir que estejam em 
+     um formato desejado ou seguro antes de serem usados em sua aplicação. */
+
+     /* filter_var() -> também aplica regras para verificar se a variável
+     está no formato correto. */
+
+     /* Uma função estática, também conhecida como método estático, 
+     é uma função dentro de uma classe em programação que pode ser invocada 
+     sem a necessidade de criar uma instância dessa classe. */
+
+    if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $erros['email'] = 'E-mail inválido.';
+    } // FILTER_VALIDATE_EMAIL é algo nativo do PHP para validar
+
+    if(!filter_var($_POST['site'], FILTER_VALIDATE_URL)) { 
+        $erros['site'] = 'Site inválido.';
+    }
+
+    $filhosConfig = ["options" => ["min_range"=>0, "max_range"=>20]];
+    
+    if(!filter_var($_POST["filhos"], FILTER_VALIDATE_INT, $filhosConfig) && $_POST['filhos'] != 0) { 
+        $erros['filhos'] = 'Quantid. Filhos inválida. (0/20)';
+    }
+    
+    $salarioConfig = ['options' => ['decimal' => ',']];
+    if (!filter_var($_POST['salario'], FILTER_VALIDATE_FLOAT, $salarioConfig)) {
+        $erros['salario'] = 'Salário inválido.';
+    }
+
+}
+
+?>
+
 <form action="#" method="post">
     <div class="form-row">
-        <div class="formgroup col-md-9">
+        <div class="formgroup col-md-8">
             <label for="nome">Nome</label>
-            <input type="text" class="form-control"
+            <input type="text" class="form-control <?= $erros['nome'] ? 'is-invalid' : '';?>"
             id="nome" name="nome" placeholder="Nome" 
             value="<?= $_POST['nome'] ?>"> <!-- Salva os valores dentro do array POST -->
+            <div class="invalid-feedback"><?= $erros['nome'] ?></div>
         </div>
 
-    <div class="formgroup col-md-3">
+    <div class="formgroup col-md-4">
         <label for="nascimento">Nascimento</label>
-        <input type="text" class="form-control"
+        <input type="text" class="form-control <?= $erros['nascimento'] ? 'is-invalid' : '';?>"
         id="nascimento" name="nascimento" placeholder="Nascimento"
         value="<?= $_POST['nascimento'] ?>">
+        <div class="invalid-feedback"><?= $erros['nascimento'] ?></div>
     </div>
 
     </div>
@@ -28,71 +89,42 @@ crossorigin="anonymous">
     <div class="form-row">
         <div class="formgroup col-md-6">
             <label for="email">E-mail</label>
-            <input type="text" class="form-control"
+            <input type="text" class="form-control <?= $erros['email'] ? 'is-invalid' : '';?>"
             id="email" name="email" placeholder="E-mail"
             value="<?= $_POST['email'] ?>">
+            <div class="invalid-feedback"><?= $erros['email'] ?></div>
         </div>
+
         <div class="formgroup col-md-6">
             <label for="site">Site</label>
-            <input type="text" class="form-control"
+            <input type="text" class="form-control <?= $erros['site'] ? 'is-invalid' : '';?>"
             id="site" name="site" placeholder="Site"
             value="<?= $_POST['site'] ?>">
+            <div class="invalid-feedback"><?= $erros['site'] ?></div>
         </div>
     </div>
     
     <div class="form-row">
         <div class="formgroup col-md-4">
             <label for="filhos">Quantid. Filhos</label>
-            <input type="text" class="form-control"
+            <input type="number" class="form-control <?= $erros['filhos'] ? 'is-invalid' : '';?>"
             id="filhos" name="filhos" placeholder="Quantid. Filhos"
-            value="<?= $_POST['filhos'] ?>">
+            value="<?= $_POST['filhos'] ?>" max=20>
+            <div class="invalid-feedback"><?= $erros['filhos'] ?></div>
         </div>
+
         <div class="formgroup col-md-8">
             <label for="salario">Salário</label>
-            <input type="text" class="form-control"
+            <input type="text" class="form-control <?= $erros['salario'] ? 'is-invalid' : '';?>"
             id="salario" name="salario" placeholder="Salário"
             value="<?= $_POST['salario'] ?>">
+            <div class="invalid-feedback"><?= $erros['salario'] ?></div>
         </div>
     </div>
     <button class="btn btn-primary btn-lg">Cadastrar</button>
 </form>
 <hr>
 
-<?php
-
-// Ao enviar o formulário o mesmo redireciona para #, sendo o próprio site, isso acontece 
-// por causa do form action="#"
-// então como o $_POST > 0 o código abaixo roda.
-
-if(count($_POST) > 0) { // Count() conta a quantidade de valores, se for maior que 0 significa
-    
-    if(!filter_input(INPUT_POST, "nome")) { // que foi criado valores dentro de post
-        echo '<p class="error">Nome é obrigatório', '<br></p>';
-    }
-    
-    if(!filter_input(INPUT_POST, "nascimento")) { // que foi criado valores dentro de post
-        echo '<p class="error">Nascimento é obrigatório', '<br></p>';
-    }
-
-    if(!filter_input(INPUT_POST, "E-mail")) { // que foi criado valores dentro de post
-        echo '<p class="error">E-mail é obrigatório', '<br></p>';
-    }
-
-    if(!filter_input(INPUT_POST, "site")) { // que foi criado valores dentro de post
-        echo '<p class="error">Site é obrigatório', '<br></p>';
-    }
-    
-    if(!filter_input(INPUT_POST, "filhos")) { // que foi criado valores dentro de post
-        echo '<p class="error">Quantid. Filhos é obrigatório', '<br></p>';
-    }
-    
-    if(!filter_input(INPUT_POST, "salario")) { // que foi criado valores dentro de post
-        echo '<p class="error">Salário é obrigatório', '<br></p>';
-    }
-
-}
-
-?>
 
 <style>
     #message {
@@ -117,17 +149,17 @@ if(count($_POST) > 0) { // Count() conta a quantidade de valores, se for maior q
     }
     form > * {
         font-size: 20px;
-        padding: 5px
+        padding: 5px;
     }
     form {
         padding-right: 20px;
         padding-left: 20px;
     }
-    .error {
-        font-size: 14px;
-        color: red;
-        margin: 0px;
-        padding: 0px;
-        text-align: center;
+    .alert {
+    font-size: 18px;
+    padding: 5px;
+    width: 50%;
+    text-align: center;
+    margin-left: 24%;
     }
 </style>
